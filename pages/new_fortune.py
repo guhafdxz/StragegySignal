c8054d3b4a6cb943772748682624e36238c168a4
smart_cypto <08lujun@gmail.com>
2024-01-24 20:37:40 +0800

    Update new_fortune.py

--- pages/new_fortune.py
+++ pages/new_fortune.py
@@ -3,5 +3,54 @@ import pandas as pd
 import numpy as np
 
 
-st.markdown("新财富🐉")
-st.sidebar.markdown("新财富🐉")
+
+st.header('加密市场新财富')
+
+col1, col2, col3,col4 = st.columns(4)
+
+with col1:
+   st.caption("Token")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+with col2:
+   st.caption("基本面")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col3:
+   st.caption("技术面")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col4:
+   st.caption("机器学习")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+col5, col6, col7,col8 =  st.columns(4)
+
+with col5:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col6:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col7:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col8:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+col9, col10, col11,col12 =  st.columns(4)
+with col9:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col10: 
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col11:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col12:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+col13, col14, col15,col16 =  st.columns(4)
+
+with col13:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col14:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col15:
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+with col16:
+    st.image("https://static.streamlit.io/examples/cat.jpg")

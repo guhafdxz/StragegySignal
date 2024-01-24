140bafb8f2155652f281ba108987945417934425
smart_cypto <08lujun@gmail.com>
2024-01-24 20:28:51 +0800

    Update crystal_ball.py

--- pages/crystal_ball.py
+++ pages/crystal_ball.py
@@ -12,72 +12,72 @@ st.header('加密市场看板')
 col1, col2, col3,col4 = st.columns(4)
 
 with col1:
-   st.header("BTC市值和统治力")
+   st.caption("BTC市值和统治力")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
-   st.header("ETH市值和汇率")
+   st.caption("ETH市值和汇率")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col3:
-   st.header("BTC和股指")
+   st.caption("BTC和股指")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col4:
-   st.header("BTC和黄金")
+   st.caption("BTC和黄金")
    st.image("https://static.streamlit.io/examples/cat.jpg")
 
 col5, col6, col7,col8 =  st.columns(4)
 
 with col5:
-    st.header("美元利率和M2")
+    st.caption("美元利率和M2")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col6:
-    st.header("USDT流动性")
+    st.caption("USDT流动性")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col7:
-    st.header("非农就业和核心CPI")
+    st.caption("非农就业和核心CPI")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col8:
-    st.header("ETF流入和流出")
+    st.caption("ETF流入和流出")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 col9, col10, col11,col12 =  st.columns(4)
 
 with col9:
-    st.header("DeFi链上TVL")
+    st.caption("DeFi链上TVL")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col10:
-    st.header("NFT成交量")
+    st.caption("NFT成交量")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col11:
-    st.header("跨链桥Bridge")
+    st.caption("跨链桥Bridge")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col12:
-    st.header("Ordinal铭文")
+    st.caption("Ordinal铭文")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 
 col13, col14, col15,col16 =  st.columns(4)
 
 with col3:
-    st.header("期货未平仓")
+    st.caption("期货未平仓")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col10:
+with col14:
     st.header("期货多空比")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col11:
-    st.header("期权波动率")
+with col15:
+    st.caption("期权波动率")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col12:
-    st.header("期权费率")
+with col16:
+    st.caption("期权费率")
     st.image("https://static.streamlit.io/examples/cat.jpg")
